1923cc456e6ab75d31988f8bfcc53013544f0bbe
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-05-21 19:13:16 -0700

    initial input binding

---
+++ Assets/Bird.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) // can fly while rotating 
+        {
+			print("flying");
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("rotating left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("rotating right");
+        }
+    }
+}
