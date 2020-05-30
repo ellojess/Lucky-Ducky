04ee6e41e5d5ce6c545341c876ee73dc733f9b25
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-05-29 19:21:05 -0700

    implement SerializeField and edit thrusts

--- Assets/Bird.cs
+++ Assets/Bird.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
-
+    [SerializeField] float rcsThrust = 100f; // turn num to float
+    [SerializeField] float mainThrust = 50f;
     Rigidbody rigidBody;
 
     // Use this for initialization
@@ -28,22 +29,29 @@ public class Bird : MonoBehaviour
         if (Input.GetKey(KeyCode.Space)) // can fly while rotating 
         {
             print("flying");
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
         }
     }
 
     private void Rotate()
     {
+        // prevent rotation movement from being as drast by taking manual control of rotation
+        rigidBody.freezeRotation = true;
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.A))
         {
             print("rotating left");
-            transform.Rotate(Vector3.forward);
+            // determine speed of rotation based on the length of frame
+         
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
             print("rotating right");
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
+        rigidBody.freezeRotation = false; // resume physics control of rotation
     }
 
 }
